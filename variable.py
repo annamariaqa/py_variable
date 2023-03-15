ed31cc6c0838672a65d2a1f437381daa932e7e84
annamariaqa <126241502+annamariaqa@users.noreply.github.com>
2023-03-15 22:07:25 +0200

    Delete variable.py

--- variable.py
+++
@@ -1,5 +0,0 @@
-property_transfer_xml = """ <con:name>AuthTocken</con:name><con:sourceType>Response</con:sourceType><con:sourceStep>login</con:sourceStep><con:sourcePath>declare namespace ns1='urn:Magento';
-//ns1:loginResponse/loginReturn[1]</con:sourcePath><con:targetType>Request</con:targetType><con:targetStep>multiCall</con:targetStep><con:targetPath>declare namespace urn='urn:Magento';
-//urn:multiCall/sessionId['?']</con:targetPath> """
-x = property_transfer_xml.split("con:targetType")[1][1:-2]
-print(x)
